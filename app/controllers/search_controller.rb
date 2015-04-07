c66fbddd4981700d78b72b964ce840909f5fd79e
legendss <david.l.castilla@gmail.com>
2015-04-07 11:27:28 -0400

    integrate google maps, fix address and zip search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,15 @@ class SearchController < ApplicationController
   end
 
   def new
-    query = {:l => params["location"], :q => params["position"],:latlong => 1, :v => 2}
+    base_uri = 'http://maps.googleapis.com/maps/api/geocode/json?'
+    api_response = HTTParty.get(base_uri, :query => {:address => params["location"]})
+    response = api_response["results"][0]["address_components"].select do |hash|
+      hash.has_value?(["postal_code"])
+    end
+    zipcode = response[0]["long_name"] if response
+    loc = zipcode || params["location"]
+    # binding.pry
+    query = {:l => loc, :q => params["position"],:latlong => 1, :v => 2, :limit => 25}
     @results = getDetails(query)["response"]["results"]["result"]
     @job = Job.new
   end
@@ -14,3 +22,5 @@ class SearchController < ApplicationController
   api_response = HTTParty.get(base_uri, :query => query)
   end
 end
+
+
