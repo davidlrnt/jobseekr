79f6391bf908d71b1f7433e6da5b220b7e55c4cd
legendss <david.l.castilla@gmail.com>
2015-04-08 16:20:57 -0400

    added carrerbuilder api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,29 +5,46 @@ class SearchController < ApplicationController
 
 
   def new
-
     ##GETS ZIPCODE || CITY
     zipcode_uri = 'http://maps.googleapis.com/maps/api/geocode/json?'
     api_response = HTTParty.get(zipcode_uri, :query => {:address => params["location"]})
-    # binding.pry
-    if api_response["results"].empty?
-      binding.pry
+
+    if api_response["results"].empty? && !params["location"].empty?
       flash[:notice] = "Invalid address"
       redirect_to root_url
     else
-
-    response = api_response["results"][0]["address_components"].select do |hash|
-      hash.has_value?(["postal_code"])
-    end
+      if !params["location"].empty?
+      response = api_response["results"][0]["address_components"].select do |hash|
+        hash.has_value?(["postal_code"])
+      end
     zipcode = response[0]["long_name"] if !response.empty?
+    else
+    zipcode = HTTParty.get("http://ipinfo.io/json")["postal"]
+    end
     loc = zipcode || params["location"]
 
 
     params["radius"] == "" ? radius = 5 : radius =  params["radius"]
+    if params["radius"] == ""
+      career_radius = 5
+    elsif params["radius"].to_i <= 7
+      career_radius = 5
+    elsif params["radius"].to_i <= 15
+      career_radius = 10
+    else params["radius"].to_i <= 25
+      career_radius = 20
+    end
+
     params["position"].delete("")
-    @results = []
+    @results = {}
     params["position"].each do |position|
-    query = {:l => loc, :q => position,:latlong => 1, :v => 2, :limit => 25, :radius => radius}
+    querycareer =  {:location => loc, :keywords => position, :radius => career_radius, :orderby => 'date', :perpage => 50 }
+    carrer_uri = 'http://api.careerbuilder.com/v1/jobsearch?DeveloperKey=WDHV4LR6B3Y8W6T8FGDB'
+    cresponse = getDetails(querycareer, carrer_uri)
+    @results[:careerbuilder] ||= []
+    @results[:careerbuilder] << cresponse["ResponseJobSearch"]["Results"]["JobSearchResult"]
+
+    query = {:l => loc, :q => position,:latlong => 1, :v => 2, :limit => 25, :sort => 'date', :radius => radius}
     indeed_uri = 'http://api.indeed.com/ads/apisearch?publisher=6706968191689061'
 
     response = getDetails(query, indeed_uri)
@@ -35,7 +52,8 @@ class SearchController < ApplicationController
         flash[:notice] = "Invalid Job"
         redirect_to root_url
       else
-      @results << response["response"]["results"]["result"]
+      @results[:indeed] ||= []
+      @results[:indeed] << response["response"]["results"]["result"]
       end
     end
     @job = Job.new
