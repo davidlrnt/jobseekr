7e1f399a6b31f1e9372eae88c372fab27821eee0
mandelbulb <mandelbulb@github.com>
2015-06-02 17:19:05 -0400

    migr change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406172332) do
+ActiveRecord::Schema.define(version: 20150406173333) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
